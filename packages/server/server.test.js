95d54e3ea494b3708e616be50e2e3d51dc09ce69
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-08-20 16:42:43 +0200

    add test GET /api

--- packages/server/server.test.js
+++ packages/server/server.test.js
@@ -1,6 +1,15 @@
 const app = require('./server');
 const request = require('supertest');
 
-test('First server test', () => {
+test('Sample test', () => {
   expect(true).toBe(true);
+});
+
+describe('Endpoints', () => {
+  it('should get /api', async done => {
+    const res = await request(app).get('/api');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toBe('GET /api');
+    done();
+  });
 });
