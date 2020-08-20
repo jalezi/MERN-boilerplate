aa7d4f78b192a90969ac4bd18bc4ad33ea438c52
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-08-20 21:31:53 +0200

    db connection & db test - mongoose

--- packages/server/server.test.js
+++ packages/server/server.test.js
@@ -1,5 +1,9 @@
-const app = require('./server');
+const mongoose = require('mongoose');
 const request = require('supertest');
+
+const { connectToDB } = require('./config');
+const app = require('./server');
+const { dbUriTest: dbUri } = require('./config');
 
 test('Sample test', () => {
   expect(true).toBe(true);
@@ -11,5 +15,28 @@ describe('Endpoints', () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body.message).toBe('GET /api');
     done();
+  });
+});
+
+describe('DB connection', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    await connectToDB();
+    connection = mongoose.connection;
+    db = connection.db;
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
+
+  it('should connects to testing db', () => {
+    const { name } = connection;
+    const { url } = connection.client.s;
+    expect(dbUri).toBe(url);
+    expect(db.databaseName).toBe(name);
   });
 });
