98e0e8ac186197f679614a582efc1884fb3cd3e0
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-08-20 13:30:01 +0200

    remove useFetch from App component

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import useFetch from './hooks/useFetch';
 
 function App() {
-  const url = 'api';
-  const options = {};
-  const res = useFetch({ url, options });
-  const { response, isLoading, error } = res;
-
-  if (isLoading) return <div>Loading....</div>;
-  if (error) {
-    const { message, stack } = error;
-    return (
-      <div>
-        ERROR:
-        <p>{message}</p>
-        <p>{stack}</p>
-      </div>
-    );
-  }
-
-  return <div>{response?.message}</div>;
+  return (
+    <div>
+      <h1>MERN boilerplate</h1>
+    </div>
+  );
 }
 
 export default App;
