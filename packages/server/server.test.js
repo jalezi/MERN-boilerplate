8f78ba4ea3ff8ee3f0b870898d64870e6830068d
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-08-21 10:57:32 +0200

    jest-mongodb issue - not sure if there is a difference

--- packages/server/server.test.js
+++ packages/server/server.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
+const fs = require('fs');
 
 const { connectToDB } = require('./config');
 const app = require('./server');
@@ -7,6 +8,17 @@ const { dbOptions } = require('./config');
 
 test('Sample test', () => {
   expect(true).toBe(true);
+});
+
+describe('Require Modules', () => {
+  it('connectToDb should be a function', () => {
+    expect(connectToDB).toBeDefined();
+    expect(typeof connectToDB).toBe('function');
+  });
+
+  it('dbOptions should be an object', () => {
+    expect(dbOptions).toBeDefined();
+  });
 });
 
 describe('Endpoints', () => {
@@ -38,6 +50,8 @@ describe('DB connection', () => {
   afterAll(async () => {
     await connection.close();
     await db.close();
+    // https://github.com/shelfio/jest-mongodb/issues/214
+    fs.unlink(process.cwd() + '/globalConfig.json', () => {});
   });
 
   it('should connects to testing db', () => {
