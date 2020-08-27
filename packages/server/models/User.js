897f9df2b4c8ff33b80e84a8aac6799872fcac4c
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-08-27 16:43:38 +0200

    add User model

---
+++ packages/server/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// Create a simple User's schema
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  gender: { type: String, required: true },
+  dob: Date,
+  loginUsing: String,
+});
+
+const User = new mongoose.model('User', userSchema);
+
+module.exports = User;
