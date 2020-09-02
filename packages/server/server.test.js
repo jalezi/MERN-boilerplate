5fa63f4cdf9c8c80ca05a143539a009463214c73
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-09-02 17:17:47 +0200

    test(server.js): createHttpServer and appListenCallback
    createHttpServer throws an error if port not number
    appListenCallback to be defined
    appListenCallback shoub be a function
    appListenCallback should throw an error
    appListenCallback should pass without an error

--- packages/server/server.test.js
+++ packages/server/server.test.js
@@ -1,10 +1,13 @@
-const createHttpServer = require('./server');
+const { createHttpServer, appListenCallback } = require('./server');
 const express = require('express');
 
-describe('Test Server', () => {
-  test('Server listen on default port', async () => {
-    let server, app;
+describe('Testing Server Port', () => {
+  let server, app;
+  beforeAll(async () => {
     app = express();
+  });
+
+  test('Server listen on default port', async () => {
     server = await createHttpServer(app);
     const port = server.address().port;
     expect(port).toBe(3000);
@@ -12,11 +15,40 @@ describe('Test Server', () => {
   });
 
   test('Server listen on port 5000', async () => {
-    let server, app;
-    app = express();
     server = await createHttpServer(app, 5000);
     const port = server.address().port;
     expect(port).toBe(5000);
     await server.close();
+  });
+
+  test('Should throw an error', async () => {
+    await expect(createHttpServer(app, 'xxxx')).rejects.toThrowError(
+      'Port is not a number'
+    );
+  });
+});
+
+describe('Testing app.listen() callback', () => {
+  test('appListenCallback should be defined', () => {
+    expect(appListenCallback).toBeDefined();
+  });
+
+  test('appListenCallback should return a function', () => {
+    const cb = appListenCallback();
+    expect(cb).toBeInstanceOf(Function);
+  });
+
+  test('Callback should throw an error', () => {
+    const cb = appListenCallback();
+    expect(() => {
+      cb(new Error('My Error'));
+    }).toThrowError('My Error');
+  });
+
+  test('Callback should pass without an error', () => {
+    const cb = appListenCallback();
+    expect(() => {
+      cb();
+    }).not.toThrowError();
   });
 });
