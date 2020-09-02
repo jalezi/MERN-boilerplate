216e1e4b60e692f8bf8105fe245d4d4df39324ac
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-09-02 14:52:59 +0200

    refactor(server): extract callback in app.listen()

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,16 +1,19 @@
+const appListenCallback = port => err => {
+  if (err) {
+    console.log('something bad happened', err);
+    // TODO Do I need: return Promise.reject(err) or something else or just logging?
+    // Promise.reject(err.message);
+    return Promise.reject(err.message);
+    // throw new Error(err.message);
+    // return new Error(err.message);
+  } else {
+    process.send = process.send || function () {};
+    process.send && process.send('online');
+    console.log(`Server is listening on ${port}`);
+  }
+};
+
 module.exports = async (app, port = 3000) => {
-  return app.listen(port, err => {
-    if (err) {
-      console.log('something bad happened', err);
-      // TODO Do I need: return Promise.reject(err) or something else or just logging?
-      // Promise.reject(err.message);
-      return Promise.reject(err.message);
-      // throw new Error(err.message);
-      // return new Error(err.message);
-    } else {
-      process.send = process.send || function () {};
-      process.send && process.send('online');
-      console.log(`Server is listening on ${port}`);
-    }
-  });
+  const cb = appListenCallback(port);
+  return app.listen(port, cb);
 };
