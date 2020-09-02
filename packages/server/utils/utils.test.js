ba8ca6a24d551ebb12ec8a813c8ba6ec8f3fca91
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-09-02 18:02:38 +0200

    test(utils): addListenersToProcess

--- packages/server/utils/utils.test.js
+++ packages/server/utils/utils.test.js
@@ -1,12 +1,27 @@
-const { isNotNodeEnvTest } = require('.');
+const { isNotNodeEnvTest, exitEvents, addListenersToProcess } = require('.');
 
-describe('', () => {
-  test('is isJestUndefined defined and function', () => {
+describe('Testing isNotNodeEnvTes', () => {
+  test('isNotNodeEnvTes defined and function', () => {
     expect(isNotNodeEnvTest).toBeDefined();
     expect(typeof isNotNodeEnvTest).toBe('function');
   });
 
-  test('isNodeEnvTest should return falsy', () => {
+  test('isNotNodeEnvTest should return falsy', () => {
     expect(isNotNodeEnvTest()).toBeFalsy();
+  });
+});
+
+describe('Testing exitEvents Object', () => {
+  it('should be defined, instance of object and frozen ', () => {
+    expect(exitEvents).toBeDefined();
+    expect(exitEvents).toBeInstanceOf(Object);
+    expect(Object.isFrozen(exitEvents)).toBeTruthy();
+  });
+});
+
+describe('Testing addListenersToProcess', () => {
+  test('somethin something', () => {
+    const exitHandler = () => () => {};
+    addListenersToProcess(exitHandler);
   });
 });
