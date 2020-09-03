006bb8bf5dd9bdf18a988bb8fe269e30612782b2
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-09-03 18:11:26 +0200

    refactor(Server Initialization):  merge 2 modules

--- packages/server/config/index.js
+++ packages/server/config/index.js
@@ -1,14 +1,29 @@
+exports.port = process.env.port || 4000; // port must match proxy settings in client package.json
+
 exports.dbUri = 'mongodb://localhost:27017/mern';
 exports.dbUriTest = 'mongodb://localhost:27017/mernTest';
 exports.dbOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
+
 exports.exitHandlerOptions = {
   coredump: false,
   timeout: 500,
 };
 
-exports.connectToDB = require('./mongoose.config');
+const connectToDB = require('./mongoose.config');
 
-exports.appInit = require('./server.config');
+const {
+  createHttpServer,
+  appInit,
+  appListenCallback,
+} = require('./server.config');
+
+exports.connectToDB = connectToDB;
+
+exports.appListenCallback = appListenCallback;
+
+exports.createHttpServer = createHttpServer;
+
+exports.appInit = appInit;
--- packages/server/config/server.config.js
+++ packages/server/config/server.config.js
@@ -1,8 +1,47 @@
-const { connectToDB, exitHandlerOptions } = require('.');
-const { createHttpServer } = require('../server');
+const connectToDB = require('./mongoose.config');
+const { port, exitHandlerOptions } = require('.');
 const { addListenersToProcess, closeServer } = require('../utils');
 
-const port = 4000; // port must match proxy settings in client package.json
+const { isNotNodeEnvTest } = require('../utils');
+const shouldConsoleLog = isNotNodeEnvTest();
+
+const appListenCallback = (port = 3000) => err => {
+  if (err) {
+    shouldConsoleLog && console.log('something bad happened', err);
+    throw new Error(err.message);
+  } else {
+    process.send = process.send || function () {};
+    process.send && process.send('online');
+    shouldConsoleLog && console.log(`Server is listening on ${port}`);
+  }
+};
+
+exports.appListenCallback = appListenCallback;
+
+const createHttpServer = async (app, port = 3000) => {
+  let cb;
+  try {
+    const isPortNotNumber = typeof port !== 'number';
+    if (isPortNotNumber) {
+      throw new Error('Port is not a number');
+    }
+    cb = appListenCallback(port);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+
+  let server;
+  try {
+    server = app.listen(port, cb);
+    return server;
+  } catch (err) {
+    console.log(err.message);
+    // throw new Error(err.message);
+    return new Error(err.message);
+  }
+};
+
+exports.createHttpServer = createHttpServer;
 
 const appInit = () => async app => {
   let server;
@@ -13,9 +52,9 @@ const appInit = () => async app => {
     addListenersToProcess(exitHandler);
   } catch (err) {
     console.log('Mongo or Server connection error', err);
-    return new Error(err.msg);
+    throw new Error(err.msg);
   }
   return server;
 };
 
-module.exports = appInit();
+exports.appInit = appInit;
