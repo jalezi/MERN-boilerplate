c8991365a1a8e14ad1220c5c70a5d6b8d17b9820
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-09-07 15:38:07 +0200

    feat(app): add auth routes with basic response

---
+++ packages/server/controllers/auth.contr.js
@@ -0,0 +1,12 @@
+module.exports.signupGet = (req, res) => {
+  res.send('signup');
+};
+module.exports.signupPost = (req, res) => {
+  res.send('new signup');
+};
+module.exports.loginGet = (req, res) => {
+  res.send('login');
+};
+module.exports.loginPost = (req, res) => {
+  res.send('user login');
+};
